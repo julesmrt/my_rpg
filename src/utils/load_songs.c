3a68c549bb04c8e03f11ab9ec1fcc4b459e41aa7
Yannis Bridon <nuwa@yannis.codes>
2019-05-04 15:42:07 +0200

    Update song loading

--- src/utils/load_songs.c
+++ src/utils/load_songs.c
@@ -11,6 +11,15 @@
 
 static const char *path = "assets/musics/";
 
+static char *get_path(const char *line)
+{
+    char *res = my_empty_char(my_strlen(line) + 1 + my_strlen(path));
+
+    res = my_strcat(res, path);
+    res = my_strcat(res, line);
+    return res;
+}
+
 music_t **load_songs(config_t *songs_config)
 {
     int size = 0;
@@ -22,17 +31,17 @@ music_t **load_songs(config_t *songs_config)
     for (int i = 0; songs_config->values[i] != NULL; i++) {
         ptr_size = sizeof(*res) * size;
         res = my_realloc(res, ptr_size, ptr_size * (size + 2));
-        temp = my_strdup(songs_config->values[i]);
         res[size] = malloc(sizeof(music_t));
-        res[size]->name = my_strdup(my_strtok(temp, ":") + 1);
-        temp_path = my_empty_char(my_strlen(path) + my_strlen(temp));
-        temp_path = my_strcat(temp_path, path);
-        temp_path = my_strcat(temp_path, temp);
+        temp = my_strdup(songs_config->values[i]);
+        res[size]->name = my_strdup(my_strtok(temp, ": "));
+        temp_path = get_path(my_strtok(NULL, ": "));
         res[size++]->ptr = sfMusic_createFromFile(temp_path);
     }
-    if (res != NULL)
-        res[size] = NULL;
-    free(temp);
+    if (res != NULL) {
+        res[size] = malloc(sizeof(music_t));
+        res[size]->name = NULL;
+        res[size]->ptr = NULL;
+    }
     free(temp_path);
     return res;
 }
