9451db1144fad09f0c3c70196540e720c0b0d2ee
Yannis Bridon <nuwa@yannis.codes>
2019-05-04 16:07:45 +0200

    Cleanup level loading, songs and texture loading

--- src/utils/load_songs.c
+++ src/utils/load_songs.c
@@ -11,12 +11,23 @@
 
 static const char *path = "assets/musics/";
 
-static char *get_path(const char *line)
+static music_t *load_single_song(const char *line)
 {
-    char *res = my_empty_char(my_strlen(line) + 1 + my_strlen(path));
+    char *cpy = my_strdup(line);
+    char *id = my_strtok(cpy, ": ");
+    char *file = my_strtok(NULL, ": ");
+    char *complete_path = my_empty_char(my_strlen(file) + my_strlen(path) + 1);
+    music_t *res = malloc(sizeof(music_t));
 
-    res = my_strcat(res, path);
-    res = my_strcat(res, line);
+    if (res == NULL) {
+        free(cpy);
+        free(id);
+        return NULL;
+    }
+    complete_path = my_strcat(complete_path, path);
+    complete_path = my_strcat(complete_path, file);
+    res->name = my_strdup(id);
+    res->ptr = sfMusic_createFromFile(complete_path);
     return res;
 }
 
@@ -24,24 +35,21 @@ music_t **load_songs(config_t *songs_config)
 {
     int size = 0;
     size_t ptr_size;
-    char *temp = NULL;
+    music_t *temp = NULL;
     music_t **res = NULL;
-    char *temp_path = NULL;
 
     for (int i = 0; songs_config->values[i] != NULL; i++) {
+        temp = load_single_song(songs_config->values[i]);
+        if (temp == NULL)
+            continue;
         ptr_size = sizeof(*res) * size;
         res = my_realloc(res, ptr_size, ptr_size * (size + 2));
-        res[size] = malloc(sizeof(music_t));
-        temp = my_strdup(songs_config->values[i]);
-        res[size]->name = my_strdup(my_strtok(temp, ": "));
-        temp_path = get_path(my_strtok(NULL, ": "));
-        res[size++]->ptr = sfMusic_createFromFile(temp_path);
+        res[size++] = temp;
     }
     if (res != NULL) {
         res[size] = malloc(sizeof(music_t));
         res[size]->name = NULL;
         res[size]->ptr = NULL;
     }
-    free(temp_path);
     return res;
 }
