e03e9953631c05811f7d7c3fb3c496c67962a3a8
Yannis Bridon <nuwa@yannis.codes>
2019-05-05 20:23:58 +0200

    Add hero loading

---
+++ src/player/load_player.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2018
+** load_player
+** File description:
+** load_player
+*/
+
+#include "my_rpg.h"
+#include <stdlib.h>
+
+hero_t *load_hero(setting_t *setting)
+{
+    hero_t *hero = malloc(sizeof(hero_t));
+
+    if (setting->type_hero == 1)
+        hero->texture = get_texture("char1", setting);
+    else
+        hero->texture = get_texture("char2", setting);
+}
