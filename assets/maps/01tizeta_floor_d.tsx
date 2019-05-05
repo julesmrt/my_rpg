b7eaa3eb1737df2c05bf8774282b363035382a88
Yannis Bridon <nuwa@yannis.codes>
2019-05-05 23:36:46 +0200

    Remove shit files

--- assets/maps/01tizeta_floor_d.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="01tizeta_floor_d" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../Downloads/01tizeta_floor_d.png" width="256" height="256"/>
-</tileset>
--- assets/maps/town_buildings.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="town_buildings" tilewidth="32" tileheight="32" tilecount="64" columns="16">
- <image source="../Downloads/town_buildings.png" width="543" height="128"/>
-</tileset>
