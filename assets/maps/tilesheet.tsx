c94699b6cc63c3abc5c2c12a38db4d6f7bdfeb41
Yannis Bridon <nuwa@yannis.codes>
2019-05-05 23:34:33 +0200

    Remove shit files

--- assets/maps/tilesheet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="tilesheet" tilewidth="32" tileheight="32" tilecount="651" columns="31">
- <image source="../tilesheet/tilesheet.png" width="993" height="673"/>
-</tileset>
